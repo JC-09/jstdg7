c2e1685fa9ee4e8c1c5ec0dbe7a47eb293303443
JC-09 <jonathancen0906@gmail.com>
2021-08-07 12:22:21 +1200

    played around with operators and string

---
+++ chapter01-jc/hello.js
@@ -0,0 +1,20 @@
+console.log("Hello JavaScript!");
+
+// Create a JavaScript object
+let book = {
+    topic: "JavaScript: The Definitive Guide.",
+    edition: 7,
+};
+
+book.author = 'Flangagan';
+console.log(`The book author is ${book.author}`);
+console.log(`The book topic is ${book.topic}`);
+console.log(`The book edition is ${book.edition}`);
+console.log(`\n\n\n\n`);
+
+
+// JavaScript Arrays
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+
