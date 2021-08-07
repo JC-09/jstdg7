b291152c609036468d9d175865c89cdfdaf629e7
JC-09 <jonathancen0906@gmail.com>
2021-08-07 13:55:00 +1200

    swapped console.log() with process.stdout.write

--- chapter01-jc/loops.js
+++ chapter01-jc/loops.js
@@ -1,3 +1,4 @@
+// require('repl').start({ prompt: '> ', writer: function myWriter() { return true } });
 let outputStr1 = "";
 for (let i = 0; i < 11; i++) { // This for-loop prints numbers from 0 to 10
     outputStr1 = outputStr1.concat(i, " ");
@@ -5,11 +6,12 @@ for (let i = 0; i < 11; i++) { // This for-loop prints numbers from 0 to 10
 console.log(outputStr1);
 
 
-let outputStr2 = "";
+// let outputStr2 = "";
 var counter = 0;
 while (true) {
-    outputStr2 = outputStr2.concat(counter, " ");
+    const isSpace = counter => { if (counter !== 10) return ' '; return "\n"; };
+    process.stdout.write(counter + isSpace(counter));
     if (counter === 10) { break; };
     counter++;
 }
-console.log(outputStr2);
+// console.log(outputStr2);
